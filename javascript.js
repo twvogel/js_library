48be765c6e4fa259dbe2a15bba65f865e360d8aa
Trevor Vogel <142553804+twvogel@users.noreply.github.com>
2026-02-14 12:24:50 -0500

    version1

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@ const myLibrary = [];
 
 const area = document.querySelector(".area");
 
-function bookInfo(title, author, pages, read) {
+
+function book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -11,18 +12,25 @@ function bookInfo(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID();
+     
+    console.log(this.id);
 
 };
 
+book.prototype.toggleStatus = function(){
+        this.read = !this.read;};
+
+
 function addBookToLibrary(title, author, pages, read) {
 
-    const book = new bookInfo(title, author, pages, read);
-    myLibrary.push(book);
-    console.log(book);
+    const newBook = new book(title, author, pages, read);
+    myLibrary.push(newBook);
+    
 };
 
 function renderLibrary(){
     area.innerHTML="";
+    console.log(myLibrary);
     myLibrary.forEach((book) => {    
         let card = document.createElement("div");
         card.classList.add("card");
@@ -36,24 +44,57 @@ function renderLibrary(){
         let page=document.createElement("p");
         page.textContent=`${book.pages} pages`;
         card.appendChild(page);
+        let status=document.createElement("p");
+        if (book.read == true) {
+            status.textContent= 'This book has been read.';}
+        else {
+            status.textContent= 'This book has not been read.';}
+        card.appendChild(status);
 
         let btn_container=document.createElement("div");
         let remove_btn=document.createElement("button");
-       
-
+        let toggle_btn=document.createElement("button");
+        
+        btn_container.classList.add("btnContainer");
+        toggle_btn.classList.add("toggle_btn");
+        toggle_btn.setAttribute("data-bookid", book.id);
+        toggle_btn.textContent="Toggle Read/Unread";
         remove_btn.classList.add("remove_btn");
-        remove_btn.setAttribute("data-bookid",`${book.id}`);
+        remove_btn.setAttribute("data-bookid", book.id);
         remove_btn.textContent="Remove";
+        btn_container.appendChild(toggle_btn);
         btn_container.appendChild(remove_btn);
         card.appendChild(btn_container);
         area.appendChild(card);
-    })
-};
+    });
+    const removeBookBtn = document.querySelectorAll('.remove_btn');
+    const toggleBookBtn = document.querySelectorAll('.toggle_btn');
+
+    for (const element of removeBookBtn)
+       {  element.addEventListener("click",(element) => {
+        const btnId = element.target.getAttribute("data-bookid");
+        const c_book = myLibrary.find(book => book.id === btnId);
+        let i_c_book = myLibrary.indexOf(c_book);
+        console.log(i_c_book);
+                myLibrary.splice(i_c_book, 1);
+                renderLibrary();});};
+
+    for (const element of toggleBookBtn)
+        { element.addEventListener("click",(element) => {
+        let bookIndex = element.target.getAttribute("data-bookid");
+        let current_book = myLibrary.find(book => book.id === bookIndex);    
+        current_book.toggleStatus();
+        renderLibrary()});
+        };
+}    
 
 const addBookBtn = document.getElementById("addBook");
 const bookDialog = document.getElementById("dialog");
 const addBookForm = document.querySelector(".submit");
 const cancelBookBtn = document.querySelector(".cancel");
+
+
+
 
 addBookBtn.addEventListener("click", () => {
     bookDialog.showModal();
@@ -64,19 +105,22 @@ cancelBookBtn.addEventListener("click", () => {
 });
 
 
-addBookForm.addEventListener("submit", (event) =>{
+addBookForm.addEventListener("click", (event) =>{
 
     event.preventDefault();
-    addBookForm.reset();
 
     const title = document.getElementById("bookTitle").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
-    const read = document.getElementById("read").checked;
+    const read = document.getElementById("read").checked ? true : false;
+    const form = document.querySelector(".form");
 
+    console.log(title, author, pages, read);
     addBookToLibrary(title, author, pages, read);
-    renderLibrary();
+    
+    form.reset();
     bookDialog.close();
+    renderLibrary();
     
 });
 
@@ -86,6 +130,8 @@ addBookToLibrary('Rich Dad Poor Dad', 'Robert Kiyosaki', 472, true);
 addBookToLibrary('The Art of War', 'Sun Tzu', 87, true);
 addBookToLibrary('Make Today Count', 'John C. Maxwell', 130, true);
 addBookToLibrary('Think and Grow Rich', 'Napoleon Hill', 345, true);
-addBookToLibrary('Way of the Wolf', 'Jordan Belfort', 248, true);
-
+addBookToLibrary('Way of the Wolf', 'Jordan Belfort', 248, false);
+addBookToLibrary('The Richest Man in Babylon', 'George S. Clason', 322, true);
 renderLibrary();
+
+
